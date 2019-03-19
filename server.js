73315624e7ccb02c5576d198cc213a5fe66d60e4
Nuno Rodrigues <nunomr@gmail.com>
2019-03-19 15:16:14 +0000

    Socket responds

--- server.js
+++ server.js
@@ -1,84 +1,120 @@
-// Run:
-// GOOGLE_APPLICATION_CREDENTIALS="./google-auth.json" node server.js
+// Run: GOOGLE_APPLICATION_CREDENTIALS="./google-auth.json" node server.js
 
-var http = require("http");
-var websocket = require("websocket");
+const http = require("http");
+const websocket = require("websocket");
 const googleCloudSpeech = require("@google-cloud/speech");
 
-var WebSocketServer = websocket.server;
+const WebSocketServer = websocket.server;
 
-var server = http.createServer(function(request, response) {
-  // process HTTP request. Since we're writing just WebSockets
-  // server we don't have to implement anything.
-  console.log("server request");
-});
-server.listen(1337, function() {
-  console.log("server started");
-});
+const PORT = 1337;
+// DEADLINE_EXCEEDED
 
-// create the server
-wsServer = new WebSocketServer({
-  httpServer: server
-});
+const speechClient = new googleCloudSpeech.SpeechClient();
 
-// WebSocket server
-wsServer.on("request", function(request) {
-  console.log("socket started");
+function createSpeechRecognizeStream() {
+  return speechClient.streamingRecognize({
+    config: {
+      encoding: "LINEAR16",
+      sampleRateHertz: 16000,
+      languageCode: "en-US"
+    },
+    interimResults: true
+  });
+}
 
-  const speechClient = new googleCloudSpeech.SpeechClient();
+function parseSpeechResults(data) {
+  return ((data && data.results) || [])
+    .filter(result => !!result)
+    .map(result => {
+      const isFinal = result.isFinal;
+      const stability = result.stability;
+      const alternatives = (result.alternatives || []).reduce((a, c) => {
+        const aConfidence = (a && a.confidence) || 0;
+        const cConfidence = (c && c.confidence) || 0;
+        return a > c ? a : c;
+      }, {});
 
-  // Create a recognize stream
-  const recognizeStream = speechClient
-    .streamingRecognize({
-      config: {
-        encoding: "LINEAR16",
-        sampleRateHertz: 16000,
-        languageCode: "en-US"
-      },
-      interimResults: true // If you want interim results, set this to true
+      if (!alternatives.transcript) {
+        return null;
+      }
+
+      return {
+        isFinal,
+        stability,
+        text: alternatives.transcript,
+        confidence: alternatives.confidence
+      };
     })
-    .on("error", console.error)
-    .on("data", data =>
-      process.stdout.write(
+    .filter(result => !!result);
+}
+
+function setupSpeechRecognizeStream({ onData, onError }) {
+  return createSpeechRecognizeStream()
+    .on("error", err => {
+      console.error("google.error", err);
+      console.error("google.error.name", err.name);
+      console.error("google.error.code", err.code);
+      console.error("google.error.message", err.message);
+
+      onError(err);
+    })
+    .on("data", data => {
+      console.log(
+        "google.data",
+        JSON.stringify(data),
         data.results[0] && data.results[0].alternatives[0]
           ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
           : `\n\nReached transcription time limit, press Ctrl+C\n`
-      )
-    );
+      );
 
-  var connection = request.accept(null, request.origin);
+      const results = parseSpeechResults(data);
 
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
-  connection.on("message", function(data) {
+      console.log(results);
+
+      onData(results);
+    });
+}
+
+const server = http.createServer((request, response) => {
+  console.log("New server request");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started listening on port ${PORT}`);
+});
+
+const wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+wsServer.on("request", request => {
+  console.log("New socket request");
+
+  const connection = request.accept(null, request.origin);
+
+  const onData = results => {
+    (results || []).forEach(result => {
+      connection.sendUTF(JSON.stringify(result));
+    });
+  };
+  const onError = err => {
+    console.log("Created new recognizeStream due to error");
+    recognizeStream = setupSpeechRecognizeStream({ onData, onError });
+  };
+
+  let recognizeStream = setupSpeechRecognizeStream({ onData, onError });
+
+  connection.on("message", data => {
     const buffer = new Int16Array(
       data.binaryData,
       0,
       Math.floor(data.byteLength / 2)
     );
-    //const buffer = Int16Array.from(data);
-    //const buffer = data;
-    //console.log(data);
-    //console.log(buffer[0], buffer[1]);
-    // Write the data chunk in the stream
     recognizeStream.write(buffer);
-    // console.log("socket message");
-    // const buffer = new Int16Array(
-    //   message,
-    //   0,
-    //   Math.floor(message.byteLength / 2)
-    // );
-    // // const buffer = new Float32Array(message);
-    // // console.log(message);
-    // // console.log(JSON.stringify(message));
-    // // console.log(buffer);
-    // // console.log(buffer[0], buffer[1]);
-    // recognizeStream.write(message);
   });
 
-  connection.on("close", function(connection) {
-    console.log("socket close", connection);
+  connection.on("close", connection => {
+    console.log("Socket closed", connection);
     recognizeStream.end();
-    // close user connection
   });
 });
